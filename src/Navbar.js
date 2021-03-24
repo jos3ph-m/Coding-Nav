6cea5734c7b4853d47879559383290219d163bba
Joseph M <jmedina1985@hotmail.com>
2021-03-23 21:12:33 -0700

    Working with the data

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,18 +15,14 @@ const Navbar = () => {
         </div>
         <div className="links-container show-container">
           <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <ul className="social-icons">
