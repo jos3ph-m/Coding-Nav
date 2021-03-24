73f7a48b05258fadfcaa1cda0052f913378245a5
Joseph M <jmedina1985@hotmail.com>
2021-03-23 21:30:36 -0700

    fixing the toggle

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,12 @@ const Navbar = () => {
   const linksRef = useRef(null);
 
   useEffect(() => {
-    const linksHeight = linksRef.current.getBoundingClientRect();
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
   }, [showLinks]);
   return (
     <nav>
