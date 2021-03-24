11c8ddcf2cfd3846795e82016c42198467e0590c
Joseph M <jmedina1985@hotmail.com>
2021-03-23 21:18:07 -0700

    fixing the toggle

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,18 +18,21 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        {showLinks && (
+          <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+
         <ul className="social-icons">
           {social.map((socialIcon) => {
             const { id, url, icon } = socialIcon;
